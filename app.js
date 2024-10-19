18e13dc25809513d55c7a27cca77148c86f89bca
CreepGantod <mosesdiorofficial@gmail.com>
2024-10-19 12:28:17 +0700

    2

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+let container = document.getElementById('container');
 
+for (let i = 0; i < 600; i++) {
+    let newDiv = document.createElement('div'); 
+    newDiv.className = "block";
+
+    container.appendChild(newDiv);
+}
